94f0235a13f19c8819c0dae1147299eca3970015
tatujan <batuhanmekiker@gmail.com>
2018-02-14 16:30:00 -0700

    programming path is added.

---
+++ appointment.py
@@ -0,0 +1,5 @@
+# parse time
+# check app. confliction
+# create class(appointmet) object
+#           name, day, start and end time, _participants
+# check if equivalent appointment_str
---
+++ event.py
@@ -0,0 +1,2 @@
+# import appointmet.py
+# include operation, time, node_id, op_param
